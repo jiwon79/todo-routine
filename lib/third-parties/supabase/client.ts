2e75ed8e5b9fd5b87ceff70399688c2d4560bcd6
jiwon79 <19-079@ksa.hs.kr>
2023-11-21 00:19:30 +0900

    :recycle: prettier 적용

--- lib/third-parties/supabase/client.ts
+++ lib/third-parties/supabase/client.ts
@@ -1,7 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/lib/third-parties/supabase/database.types";
+import { createClient } from '@supabase/supabase-js';
+import { Database } from '@/lib/third-parties/supabase/database.types';
 
-const supabaseURL = process.env["NEXT_PUBLIC_SUPABASE_URL"] ?? ''
-const supabaseAnonKey = process.env["NEXT_PUBLIC_SUPABASE_ANON_KEY"] ?? ''
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
-export const supabaseClient = createClient<Database>(supabaseURL, supabaseAnonKey)
+export const supabaseClient = createClient<Database>(
+  supabaseURL,
+  supabaseAnonKey,
+);
--- lib/third-parties/supabase/database.types.ts
+++ lib/third-parties/supabase/database.types.ts
@@ -4,38 +4,38 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       Users: {
         Row: {
-          created_at: string
-          id: number
-        }
+          created_at: string;
+          id: number;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-        }
+          created_at?: string;
+          id?: number;
+        };
         Update: {
-          created_at?: string
-          id?: number
-        }
-        Relationships: []
-      }
-    }
+          created_at?: string;
+          id?: number;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
