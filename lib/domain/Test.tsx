fff5856aa67d4bb534ec54a00f49d2e2a41d1199
jiwon79 <19-079@ksa.hs.kr>
2023-11-26 11:29:30 +0900

    :heavy_minus_sign: auth-helper 삭제

--- lib/domain/Test.tsx
+++ lib/domain/Test.tsx
@@ -7,11 +7,20 @@ import {
 } from '@/lib/third-parties/supabase';
 
 export const Test = () => {
+  const onClickSession = async () => {
+    const { data, error } = await getSession();
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(data);
+  };
+
   return (
     <>
       <button onClick={signInWithKakao}>카카오 로그인</button>
       <button onClick={signOut}>로그아웃</button>
-      <button onClick={getSession}>세션</button>
+      <button onClick={onClickSession}>세션</button>
     </>
   );
 };
