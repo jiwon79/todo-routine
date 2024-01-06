fbb449c489df561dc0313264998d4799e1e562c5
jiwon79 <19-079@ksa.hs.kr>
2024-01-06 21:15:58 +0900

    :sparkles: Client, server에서 유저 정보를 받을 수 있는 Provider 추가

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,11 @@
 import { MongooseService, UsersModel } from '@/lib/third-parties/mongoose';
+import { authOptions } from '@/lib/third-parties/next-auth/authOptions';
+import { getServerSession } from 'next-auth';
 
 export const GET = async () => {
+  const session = await getServerSession(authOptions);
+  console.log('server session', session);
+
   await MongooseService.connect();
   const query = UsersModel.find();
   const docs = await query;
