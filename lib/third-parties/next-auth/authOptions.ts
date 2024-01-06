ef375db78b56efea72aecdc7e694884b2bf05070
jiwon79 <19-079@ksa.hs.kr>
2024-01-06 21:15:38 +0900

    :sparkles: next-auth route 추가

---
+++ lib/third-parties/next-auth/authOptions.ts
@@ -0,0 +1,17 @@
+import { NextAuthOptions } from 'next-auth';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log('signIn', user, account, profile);
+      return true;
+    },
+  },
+};
