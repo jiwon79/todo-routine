51192667d86f8ea88cf6fd83e744a397151241d6
jiwon79 <19-079@ksa.hs.kr>
2024-01-11 22:37:59 +0900

    ♻️ delete unused

--- lib/domain/auth/server/actions/signIn/signInEmailPwd.ts
+++ lib/domain/auth/server/actions/signIn/signInEmailPwd.ts
@@ -1,6 +1,6 @@
 import { SignInEmailPwdBody } from '@/lib/domain/auth/interface';
 import { MongooseService, UserModel } from '@/lib/third-parties/mongoose';
-import { compareHash, hash } from '@/lib/third-parties/bcrypt';
+import { compareHash } from '@/lib/third-parties/bcrypt';
 import { NextResponse } from 'next/server';
 
 interface SignInEmailPwdRequest extends Request {
@@ -10,14 +10,12 @@ interface SignInEmailPwdRequest extends Request {
 export async function signInEmailPwd(req: SignInEmailPwdRequest) {
   const { email, password } = await req.json();
   await MongooseService.connect();
-  console.log(email, password);
   const users = await UserModel.find({ email });
   if (users.length === 0) {
     return NextResponse.json({ error: 'NOT_CORRECT_EMAIL' }, { status: 400 });
   }
 
   const user = users[0]!;
-  console.log('user', user);
   if (user.password && !compareHash(password, user.password)) {
     return NextResponse.json({ error: 'NOT_CORRECT_EMAIL' }, { status: 400 });
   }
